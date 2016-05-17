d774a1b94548888744896bfe8bf66dc7df53e828
Nanda <nandakishore86@gmail.com>
2016-05-17 13:18:26 +0530

    Added all new files

---
+++ src/test/java/com/nk/common/common_pages.java
@@ -0,0 +1,22 @@
+package com.nk.common;
+
+import org.openqa.selenium.WebDriver;
+
+public class common_pages {
+	
+	public WebDriver uidriver;
+	
+	public common_pages(WebDriver driver)
+	{
+		this.uidriver = driver;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
